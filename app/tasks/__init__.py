c2ca21e1b5b73d105b9de4687322593c1d8181fc
Kitasola <sorakita0221@gmail.com>
2025-04-15 17:10:34 +0000

    非同期処理を使用してタスクの登録方法を改善

--- app/tasks/__init__.py
+++ app/tasks/__init__.py
@@ -1,7 +1,10 @@
 from .event_tasks import monitor_event_participants, cleanup_old_events
+import asyncio
+from asyncio import get_event_loop
 
 def register_tasks(client, scheduler):
-    scheduler.add_job(lambda: monitor_event_participants(client), 'interval', minutes=1)  # 1分ごとに実行
-    scheduler.add_job(lambda: cleanup_old_events(client, scheduler), 'cron', hour=5)  # 毎日午前5時に実行
+    loop = get_event_loop()
+    scheduler.add_job(lambda: asyncio.run_coroutine_threadsafe(monitor_event_participants(client), loop), 'interval', minutes=1)  # 1分ごとに実行
+    scheduler.add_job(lambda: asyncio.run_coroutine_threadsafe(cleanup_old_events(client, scheduler), loop), 'cron', hour=5)  # 毎日午前5時に実行
 
 __all__ = ["register_tasks"]

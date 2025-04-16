aa2c54bb370da0c1c7f3105fc5e1202bd95713ad
Kitasola <sorakita0221@gmail.com>
2025-04-16 21:50:37 +0000

    イベント監視機能をボタン処理に移植したため、タスクを削除

--- app/tasks/__init__.py
+++ app/tasks/__init__.py
@@ -1,5 +1,5 @@
 # recruitment.pyからタスク関数をインポート
-from .recruitment import monitor_event_participants, cleanup_old_events
+from .recruitment import cleanup_old_events
 import asyncio
 from asyncio import get_event_loop
 
@@ -7,10 +7,8 @@ from asyncio import get_event_loop
 def register_tasks(client, scheduler):
     # 非同期イベントループを取得
     loop = get_event_loop()
-    # 1分ごとに参加者を監視するタスクをスケジューラに追加
-    scheduler.add_job(lambda: asyncio.run_coroutine_threadsafe(monitor_event_participants(client), loop), 'interval', minutes=1)
     # 毎日午前5時に古いイベントを削除するタスクをスケジューラに追加
-    scheduler.add_job(lambda: asyncio.run_coroutine_threadsafe(cleanup_old_events(client, scheduler), loop), 'cron', hour=5)
+    scheduler.add_job(lambda: asyncio.run_coroutine_threadsafe(cleanup_old_events(client), loop), 'cron', hour=5)
 
 # モジュールとして公開する関数を定義
 __all__ = ["register_tasks"]

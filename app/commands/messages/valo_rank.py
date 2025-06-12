f1237908f76a1b555867a0fa7463645fec00001c
Kitasola <sorakita0221@gmail.com>
2025-06-12 21:23:57 +0000

    登録ボタン追加

--- app/commands/messages/valo_rank.py
+++ app/commands/messages/valo_rank.py
@@ -18,7 +18,7 @@ class RankDivSelectView(View):
             message = "現在のランク: 未登録"
         else:
             message = f"現在のランク: {result[0]} {result[1]}"
-        await self.interaction.response.send_message(content=message, view=self)
+        await self.interaction.response.send_message(content=message, view=self, ephemeral=True)
 
     @discord.ui.select(
         placeholder="Rankを選択してください",
@@ -26,12 +26,10 @@ class RankDivSelectView(View):
         custom_id="rank_select"
     )
     async def rank_select(self, interaction: discord.Interaction, select: discord.ui.Select):
+        await interaction.response.defer()
         self.rank = select.values[0]
         if self.rank == "Radiant":
             self.div = "1"
-            await interaction.response.send_message(f"{self.rank} を選択しました。divの選択は必要ありません。", ephemeral=True)
-        else:
-            await interaction.response.defer()
 
     @discord.ui.select(
         placeholder="Divを選択してください",
@@ -39,9 +37,24 @@ class RankDivSelectView(View):
         custom_id="div_select"
     )
     async def div_select(self, interaction: discord.Interaction, select: discord.ui.Select):
+        await interaction.response.defer()
         self.div = select.values[0]
+
+    @discord.ui.button(label="登録", style=discord.ButtonStyle.primary)
+    async def register_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        # ユーザーがRankとDivを選択しているか確認
+        if self.rank is None or self.div is None:
+            await interaction.response.send_message("RankとDivを選択してください。", ephemeral=True)
+            return
+
+        # RankがRadiantの場合、Divは1に固定
         if self.rank == "Radiant":
             self.div = "1"
-            await interaction.response.send_message(f"{self.rank} を選択しました。", ephemeral=True)
-        else:
-            await interaction.response.send_message(f"{self.rank} {self.div} を選択しました。", ephemeral=True)
+
+        # データベースに登録
+        cursor.execute(
+            "INSERT OR REPLACE INTO user_info (guild_id, user_id, rank, div) VALUES (?, ?, ?, ?)",
+            (self.interaction.guild.id, self.interaction.user.id, self.rank, self.div)
+        )
+        cursor.connection.commit()
+        await interaction.response.send_message(f"{self.rank} {self.div} で登録しました。", ephemeral=True)

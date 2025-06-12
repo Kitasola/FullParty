202b7dbd5b51cf3194c541d29633cb57e22fcd0b
Kitasola <sorakita0221@gmail.com>
2025-06-12 11:03:26 +0000

    ランク定数を外だし

--- app/commands/logic/valo_team.py
+++ app/commands/logic/valo_team.py
@@ -1,24 +1,5 @@
 from pulp import LpProblem, LpVariable, lpSum
-
-VALO_RANK = {
-    "Iron": 0,
-    "Bronze": 1,
-    "Silver": 2,
-    "Gold": 3,
-    "Platinum": 4,
-    "Diamond": 5,
-    "Ascendant": 6,
-    "Immortal": 7,
-    "Radiant": 8,
-}
-
-VALO_DIV = {
-    "1": 0,
-    "2": 1,
-    "3": 2,
-}
-
-VALO_TEAM_SIZE = 5  # チームのサイズ
+from utils import VALO_RANK, VALO_DIV, VALO_TEAM_SIZE
 
 async def create(user_info: list, team_num: int = 2) -> dict:
     # ユーザー情報の検証
---
+++ app/utils/__init__.py
@@ -0,0 +1,1 @@
+from .valo_rank import *
